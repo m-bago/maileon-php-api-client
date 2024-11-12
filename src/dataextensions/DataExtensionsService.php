2739ff0517e1a8fb24f3562b8cddf9480dd052fb
mbago <martin.bago@xqueue.com>
2024-11-12 15:11:21 +0100

    implement getting Data Extension by extension ID

--- src/dataextensions/DataExtensionsService.php
+++ src/dataextensions/DataExtensionsService.php
@@ -3,13 +3,31 @@
 namespace de\xqueue\maileon\api\client\dataextensions;
 
 use de\xqueue\maileon\api\client\AbstractMaileonService;
+use de\xqueue\maileon\api\client\MaileonAPIResult;
 
 class DataExtensionsService extends AbstractMaileonService
 {
     private $mimeType = "application/vnd.maileon.api+json";
 
-    public function getDataTypes()
+    public function getDataTypes(): MaileonAPIResult
     {
         return $this->get("dataextensions/datatypes", [], $this->mimeType);
+    }
+
+    //TODO implement properly
+//    public function deleteDataExtension(string $dataExtensionId)
+//    {
+//        $queryParameters = [
+//            'id' => $dataExtensionId
+//        ];
+//        return $this->delete('dataextensions/', $queryParameters, );
+//    }
+
+    public function getDataExtension(string $dataExtensionId)
+    {
+        $queryParameters = [
+            'id' => $dataExtensionId
+        ];
+        return $this->get("dataextensions/", $queryParameters, $this->mimeType);
     }
 }
