68a590f6eadf08a3fa861007b1b7410229e93b2c
mbago <martin.bago@xqueue.com>
2024-11-12 13:56:24 +0100

    implement very early version of getting available Data Types

--- src/dataextensions/DataExtensionsService.php
+++ src/dataextensions/DataExtensionsService.php
@@ -2,7 +2,14 @@
 
 namespace de\xqueue\maileon\api\client\dataextensions;
 
-class DataExtensionsService
-{
+use de\xqueue\maileon\api\client\AbstractMaileonService;
 
+class DataExtensionsService extends AbstractMaileonService
+{
+    private $mimeType = "application/vnd.maileon.api+json";
+
+    public function getDataTypes()
+    {
+        return $this->get("dataextensions/datatypes", [], $this->mimeType);
+    }
 }
